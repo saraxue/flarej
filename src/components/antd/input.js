0213a73476e9d27036c705a1329a52932ad177c2
chenxiaochun <sjzchenxiaochun@gmail.com>
2018-05-22 09:52:16 +0800

    Update input.js

--- src/components/antd/input.js
+++ src/components/antd/input.js
@@ -5,7 +5,8 @@ import 'antd/lib/input/style/index';
 registerComponent({
   'ant-Input': Input,
   'ant-InputGroup': Input.Group,
-  'ant-TextArea': Input.TextArea
+  'ant-TextArea': Input.TextArea,
+  'ant-Search': Input.Search
 });
 
 export {
